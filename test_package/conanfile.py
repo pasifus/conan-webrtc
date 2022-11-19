c9dfb60e4d5bb3731ea9c482d44909385b62c0ef
pasifus <pasifus@gmail.com>
2022-11-19 19:14:33 +0200

    fix #8

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,29 +1,25 @@
 from conans import ConanFile, CMake, tools
 import os
 
+
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake", "cmake_find_package"
 
-    # # https://docs.conan.io/en/latest/reference/conanfile/methods.html#configure
-    # def configure(self):
-    #     # add "d" (if not exist) to "Visual Studio" compile runtime
-    #     if settings["compiler"] == "Visual Studio" and settings["build_type"] == "Debug":
-    #         if not settings["compiler.runtime"].endswith("d"):
-    #             settings["compiler.runtime"] += "d"
+    def system_requirements(self):
+        packages = []
+        if tools.os_info.is_linux and self.settings.os == "Linux":
+            packages = ["libasound2-dev", "libpulse-dev", "libxdamage-dev", "libxrandr-dev", "libxtst-dev", "libxcomposite-dev", "libgbm-dev"]
+        if packages:
+            package_tool = tools.SystemPackageTool(conanfile=self, default_mode='verify')
+            for p in packages:
+                package_tool.install(update=True, packages=p)        
 
-    # # https://docs.conan.io/en/latest/reference/conanfile/methods.html#requirements
-    # def requirements(self):
-    #     self.requires("glib/2.69.1")
-
-    # https://docs.conan.io/en/latest/reference/conanfile/methods.html#build
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["CONAN_DISABLE_CHECK_COMPILER"] = "True" # skip cheching compile version
         cmake.configure()
         cmake.build()
 
-    # https://docs.conan.io/en/latest/reference/conanfile/methods.html#test
     def test(self):
         if not tools.cross_building(self.settings):
             bin_path = os.path.join(os.getcwd(), "bin", "test_package")
