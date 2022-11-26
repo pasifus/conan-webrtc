7e088f3f7b32fd17bcbaa1c5c47ba727269fdc43
pasifus <pasifus@gmail.com>
2022-11-26 18:03:31 +0200

    fix #16

--- conanfile.py
+++ conanfile.py
@@ -15,8 +15,8 @@ class Webrtc(ConanFile):
     generators = []
     exports_sources = [".gclient", "patches/**"]
     short_paths = True
-    revision_mode = "scm"  
-    
+    revision_mode = "scm"
+
     @property
     def _depot_tools_dir(self):
         return os.path.join(self.source_folder, "depot_tools")
@@ -33,7 +33,8 @@ class Webrtc(ConanFile):
     def source(self):
         # download and configure depot_tools
         git_depot_tools = tools.Git(folder="depot_tools")
-        git_depot_tools.clone(self.conan_data["sources"][self.version][0]["url"], self.conan_data["sources"][self.version][0]["branch"])
+        git_depot_tools.clone(self.conan_data["sources"][self.version][0]["url"],
+                              self.conan_data["sources"][self.version][0]["branch"])
         # download and configure webrtc
         with tools.environment_append({"PATH": [self._depot_tools_dir], "DEPOT_TOOLS_WIN_TOOLCHAIN": "0"}):
             self.run("echo 'target_os = %s' >> .gclient" % self._gclient_os())
@@ -43,10 +44,10 @@ class Webrtc(ConanFile):
 
     def _patches(self):
         self.output.info("starting patches to webrtc")
-                        
+
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
-        
+
         # https://groups.google.com/forum/#!topic/discuss-webrtc/f44XZnQDNIA
         # https://stackoverflow.com/questions/49083754/linking-webrtc-with-qt-on-windows
         # https://docs.conan.io/en/latest/reference/tools.html#tools-replace-in-file
@@ -60,7 +61,7 @@ class Webrtc(ConanFile):
             with tools.chdir(self._webrtc_source_dir):
                 clockdrift_detector_file = os.path.join(
                     'modules', 'audio_processing', 'aec3', 'clockdrift_detector.h')
-                # missing `std::` wont compile with gcc10
+                # missing `std::` won't compile with gcc10
                 tools.replace_in_file(clockdrift_detector_file,
                                       ' size_t stability_counter_;',
                                       ' std::size_t stability_counter_;')
@@ -68,18 +69,20 @@ class Webrtc(ConanFile):
         # there is a `include <cstring>` missing when not compiling with
         # their stdcxx (`use_custom_libcxx`)
         with tools.chdir(self._webrtc_source_dir):
-            stack_copier_signal_file = os.path.join( 'base', 'profiler', 'stack_copier_signal.cc')
+            stack_copier_signal_file = os.path.join('base', 'profiler', 'stack_copier_signal.cc')
             tools.replace_in_file(stack_copier_signal_file,
                                   '#include <syscall.h>',
                                   '''#include <syscall.h>
                                      #include <cstring>''')
-        
+
     def _gclient_revision(self):
         from six import StringIO
         stdout = StringIO()
-        self.run("git ls-remote %s --heads %s | head -n 1 | cut -f 1" % (self.conan_data["sources"][self.version][1]["url"],
-                                                      self.conan_data["sources"][self.version][1]["branch"]),  
-                                                      output=stdout)
+        self.run(
+            "git ls-remote %s --heads %s | head -n 1 | cut -f 1" % (self.conan_data["sources"][self.version][1]["url"],
+                                                                    self.conan_data["sources"][self.version][1][
+                                                                        "branch"]),
+            output=stdout)
         return stdout.getvalue()
 
     def _gn_os(self):
@@ -122,8 +125,8 @@ class Webrtc(ConanFile):
             args.extend(['rtc_with_h264=true', 'proprietary_codecs=true', 'ffmpeg_branding="Chrome"'])
         # add args by os
         if self.settings.os == "Windows":
-            args.extend(['use_custom_libcxx=false', # 'is_clang=false', 
-                         'treat_warnings_as_errors=false']) # , 'visual_studio_version=\\"2019\\" visual_studio_path=\\"C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\"'
+            args.extend(['use_custom_libcxx=false',
+                         'treat_warnings_as_errors=false'])
             if self.settings.build_type == "Debug":
                 # if not set the compilation will fail with:
                 # _iterator_debug_level value '0' doesn't match value '2'
@@ -135,7 +138,8 @@ class Webrtc(ConanFile):
         elif self.settings.os == "Macos":
             args.extend(['is_clang=true', 'use_custom_libcxx=false', 'enable_dsyms=true'])
         elif self.settings.os == "iOS":
-            args.extend(['is_clang=true', 'use_custom_libcxx=false', 'enable_dsyms=true', 'ios_enable_code_signing=false'])
+            args.extend(
+                ['is_clang=true', 'use_custom_libcxx=false', 'enable_dsyms=true', 'ios_enable_code_signing=false'])
         else:
             raise Exception("not valid os: ", self.settings.os)
         return " ".join(args)
@@ -197,16 +201,19 @@ class Webrtc(ConanFile):
         # platform specific
         if self.settings.os == "Windows":
             self.cpp_info.defines.extend(['WEBRTC_WIN', 'WIN32_LEAN_AND_MEAN', 'NOMINMAX'])
-            self.cpp_info.system_libs.extend(["secur32", "winmm", "dmoguids", "wmcodecdspuuid", "msdmo", "Strmiids", "Iphlpapi.lib"])
+            self.cpp_info.system_libs.extend(
+                ["secur32", "winmm", "dmoguids", "wmcodecdspuuid", "msdmo", "Strmiids", "Iphlpapi.lib"])
         elif self.settings.os == "Linux":
             self.cpp_info.system_libs = ["dl"]
             self.cpp_info.defines.extend(["WEBRTC_POSIX", "WEBRTC_LINUX"])
-            self.cpp_info.system_libs.extend(["Xtst", "Xfixes", "Xrandr", "Xext", "Xcomposite", "Xdamage", "X11", "glib-2.0", "pthread"])
+            self.cpp_info.system_libs.extend(
+                ["Xtst", "Xfixes", "Xrandr", "Xext", "Xcomposite", "Xdamage", "X11", "glib-2.0", "pthread"])
         elif self.settings.os == "Macos":
             self.cpp_info.defines.extend(["WEBRTC_POSIX", "WEBRTC_MAC"])
             self.cpp_info.frameworks.extend(["Foundation", "CoreServices", "CoreFoundation", "AppKit", "IOSurface"])
         elif self.settings.os == "iOS":
             self.cpp_info.defines.extend(["WEBRTC_POSIX", "WEBRTC_IOS", "WEBRTC_MAC"])
-            self.cpp_info.frameworks.extend(["AVFoundation", "CFNetwork", "Foundation", "Security", "SystemConfiguration", "UIKit"])
+            self.cpp_info.frameworks.extend(
+                ["AVFoundation", "CFNetwork", "Foundation", "Security", "SystemConfiguration", "UIKit"])
         else:
             raise Exception("not valid os: ", self.settings.os)
